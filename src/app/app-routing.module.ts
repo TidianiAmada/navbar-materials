3c949e4d874e4596f1950175962aa224f13bbb90
TidianiAmada <tidiani.amada@gmail.com>
2022-01-03 18:36:25 +0100

    navbar with A.M

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DummyComponent } from './dummy/dummy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:DummyComponent},
+  {path:'about', component:DummyComponent},
+  {path:'contact', component:DummyComponent},
+  {path:'signin', component:DummyComponent},
+  {path:'login', component:DummyComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
